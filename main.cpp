18d9e71ef8ecb1b46e824bfcc7e8a5643a4b0a21
Roman <exculibur@rambler.ru>
2017-11-05 15:12:14 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <experimental/filesystem>
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+namespace fs = experimental::filesystem;
+
+int main() {
+	setlocale(LC_ALL, "rus");
+
+	try {
+		string address;
+		cout << endl
+			<< "Укажите путь к файлу:" << endl
+			<< "Пример: 'explorer/file.dat' " << endl << endl;
+		cout << "Ввод:\t";
+		cin >> address;
+
+		if (!fs::exists(address))
+			throw runtime_error("Ошибка: Такого адреса не существует!");
+
+		const fs::path filePath = address;
+
+		const fs::path textFilename = filePath.filename();
+
+		auto ftime = fs::last_write_time(filePath);
+
+		time_t cftime = decltype(ftime)::clock::to_time_t(ftime);
+
+		cout << endl;
+		cout << "name:\t\t" << textFilename << endl;
+		cout << "size:\t\t" << (double(fs::file_size(filePath)) / 1000) << " Kb" << endl;
+		cout << "date_mod:\t" << std::asctime(std::localtime(&cftime)) << endl;
+
+	}
+	catch (const std::exception& e) {
+		cout << e.what() << endl;
+	}
+
+	system("pause");
+	return 0;
+}
