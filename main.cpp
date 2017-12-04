ff069b4614e99b87e9c704b572ddb9b44fb673ba
Roman <exculibur@rambler.ru>
2017-12-04 21:57:57 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,36 +6,36 @@ using namespace std;
 namespace fs = experimental::filesystem;
 
 int main() {
-	setlocale(LC_ALL, "rus");
+   setlocale(LC_ALL, "rus");
 
-	try {
-		string address;
-		cout << endl;
-		cout << "Укажите путь к файлу:" << endl;
-		cout << "Пример: 'explorer/file.dat' " << endl << endl;
-		cout << "Ввод:\t";
-		cin >> address;
+   try {
+	string address;
+	cout << endl;
+	cout << "Укажите путь к файлу:" << endl;
+	cout << "Пример: 'explorer/file.dat' " << endl << endl;
+	cout << "Ввод:\t";
+	cin >> address;
 
-		if (!fs::exists(address))
-			throw runtime_error("Ошибка: Такого адреса не существует!");
+	if (!fs::exists(address))
+		throw runtime_error("Ошибка: Такого адреса не существует!");
 
-		const fs::path filePath = address;
+	const fs::path filePath = address;
 
-		const fs::path textFilename = filePath.filename();
+	const fs::path textFilename = filePath.filename();
 
-		auto ftime = fs::last_write_time(filePath);
+	auto ftime = fs::last_write_time(filePath);
 
-		time_t cftime = decltype(ftime)::clock::to_time_t(ftime);
+	time_t cftime = decltype(ftime)::clock::to_time_t(ftime);
 
-		cout << endl;
-		cout << "name:\t\t" << textFilename << endl;
-		cout << "size:\t\t" << (double(fs::file_size(filePath)) / 1000) << " Kb" << endl;
-		cout << "date_mod:\t" << std::asctime(std::localtime(&cftime)) << endl;
+	cout << endl;
+	cout << "name:\t\t" << textFilename << endl;
+	cout << "size:\t\t" << (double(fs::file_size(filePath)) / 1000) << " Kb" << endl;
+	cout << "date_mod:\t" << std::asctime(std::localtime(&cftime)) << endl;
 
-	}
-	catch (const std::exception& e) {
-		cout << e.what() << endl;
-	}
+}
+catch (const std::exception& e) {
+	cout << e.what() << endl;
+}
 
-	return 0;
+return 0;
 }
