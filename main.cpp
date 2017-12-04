f69f42097d54f823ae127697ca8c48ab7eddea90
Roman <exculibur@rambler.ru>
2017-12-04 22:49:46 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 #include <iostream>
 #include <cstring>
 
-using namespace std;
-namespace fs = experimental::filesystem;
+using namespace std; // namespace std
+namespace fs = experimental::filesystem; // namespace fs
 
 int main() {
    setlocale(LC_ALL, "rus");
