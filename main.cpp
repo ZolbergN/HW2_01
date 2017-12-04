add53a3f5df6119c974cdd34c53bbf7cd002a2a3
Roman <exculibur@rambler.ru>
2017-12-04 23:10:12 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -28,10 +28,8 @@ int main() {
 
         std::cout << std::endl;
         std::cout << "name:\t\t" << textFilename << std::endl;
-        std::cout << "size:\t\t" << (double(fs::file_size(filePath)) / 1000) << " Kb" 
-           << std::endl;
-        std::cout << "date_mod:\t" << std::asctime(std::localtime(&cftime)) 
-           << std::endl;
+        std::cout << "size:\t\t" << (double(fs::file_size(filePath)) / 1000) << " Kb" << std::endl;
+        std::cout << "date_mod:\t" << std::asctime(std::localtime(&cftime)) << std::endl;
 
 }
 catch (const std::exception& e) {
